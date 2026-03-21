fd975c7a4dddb9cd38498468f796c837cf2228bb
Dilukshan <65407969+dilukshann7@users.noreply.github.com>
2026-03-21 19:12:04 +0530

    feat: Implement custom font 'Relationship of mélodrame'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Relationship of mélodrame",
+      cssVariable: "--font-melodrame",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/font/font.ttf"],
+            weight: "normal",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
 });
