5f9eb98221588aa552546e3d36c027e583db862f
Dilukshan <65407969+dilukshann7@users.noreply.github.com>
2026-03-28 11:52:02 +0530

    chore(colors): Standardize import quotes

--- src/components/ui/ColorBends.tsx
+++ src/components/ui/ColorBends.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef } from 'react';
-import * as THREE from 'three';
-import './ColorBends.css';
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+import "./ColorBends.css";
 
 type ColorBendsProps = {
   className?: string;
